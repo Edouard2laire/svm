3fa763c60cf8bb74181723869f0cde3acab2e9c8
Edouard Delaire <edouard.delaire@icloud.com>
2017-07-29 21:51:28 +0200

    Utilisation de la nouvelle interface et nettoyage du code

--- SDCA.py
+++ SDCA.py
@@ -1,27 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from random import randint
-from heapq import merge
 from time import time
-"""
-kile
 
 
 
-"""
-class SDCA :
+class SDCA(svm) :
     name="SDCA"
-    """ Attributs : 
-        -fonction de noyeau
-        -liste des alpha
-        -gamma
-        
-        -fonctions : 
-            -train 
-            -predict    
-    """
-    def __init__(self, lkernel):
+
+    def __init__(self, lkernel,E=10,C=100,gamma=2):
+        svm.__init__(self)
         self.noyau=lkernel
+        self.E=E
+        self.C=C
+        self.gamma=gamma
         self.x = None
         self.y = None
 
@@ -46,54 +38,29 @@ class SDCA :
         return K
 
 
-    def train(self,x,y,E=10,C=100,gamma=2):
-        self.gamma=gamma
+    def fit(self,x,y):
         self.x = x
         self.y = y
         n = np.size(y)
         K=self.kernel(self.noyau, x)
 
-        """
-        plt.figure(2)
-        plt.imshow(K)
-        plt.show()
-        """
+
         y_hat = [0] * n
         alpha = [0] * n
-        e = 0
+        e = 1
         t = 0
 
-        while e < E:
+        while e <= self.E:
             L = np.random.permutation(range(n))
             for i in L:
                 g = 1 - y[i] * y_hat[i]
-                if not (g == 0 or (g > 0 and alpha[i] == C) or (g < 0 and alpha[i] == 0)):
-                    alpha_new = max(0, min(alpha[i] + g / K[i][i], C))
+                if not (g == 0 or (g > 0 and alpha[i] == self.C) or (g < 0 and alpha[i] == 0)):
+                    alpha_new = max(0, min(alpha[i] + g / K[i][i], self.C))
                     delta = alpha_new - alpha[i]
                     for j in range(0, n):
                         y_hat[j] = y_hat[j] + delta * y[i] * K[i][j]
                     alpha[i] = alpha_new
 
-                    """
-                    self.alpha = alpha
-                    
-                    fig = plt.figure(1)
-                    fig.canvas.set_window_title("iteration {}".format(t))
-                    t+=1
-                    plt.clf()
-                    axes = plt.gca()
-                    axes.set_xlim([-3,6])
-                    axes.set_ylim([-2,2])
-
-                    plt.stem(x, [y[i] * alpha[i] for i in range(len(alpha))], markerfmt='b+')
-
-                    xtest = np.linspace(-3, 6, 80)
-                    ytest = [gaussian.predict(xi) for xi in xtest]
-                    plt.plot(xtest, ytest)
-                    plt.show()
-
-                    plt.pause(0.3)
-                    """
             e = e + 1
 
         self.alpha=alpha
@@ -105,93 +72,3 @@ class SDCA :
             y+= self.alpha[i]*self.y[i]*self.noyau(x, self.x[i], self.gamma)
 
         return y
-
-    def test(self,x,y):
-        c=0.0
-        for i,xi in enumerate(x):
-            if( int(np.sign(self.predict(xi))) == int(np.sign(y[i]))) :
-                c+=1.0
-
-        return c/len(x)
-
-
-
-
-
-"""  liste des fichiers :
-"/users/edoudela12/PycharmProjects/Edouard/diabetes_scale"
-"/users/edoudela12/PycharmProjects/Edouard/heart_scale"
-"/users/edoudela12/PycharmProjects/Edouard/ionosphere_scale"
-"/users/edoudela12/PycharmProjects/Edouard/sonar_scale"
-"""
-"""
-
-x,y=load("/users/edoudela12/PycharmProjects/Edouard/diabetes_scale")
-
-n=np.shape(x)[0]
-
-acc = 0.0
-t=0.0
-ntest=20
-for i in range(ntest):
-    p = np.random.permutation(range(n))
-
-    m = int(0.7*n)
-    x_train = x[p[1:m]]
-    y_train = y[p[1:m]]
-    x_test = x[p[m:-1]]
-    y_test = y[p[m:-1]]
-
-
-    gaussian=SDCA(gauss)
-    #gaussian.gamma_id(x_train)
-    gaussian.gamma = 1.0
-    t1=time()
-    gaussian.train(x_train,y_train,E=100,C=100)
-    t2=time()
-    r=gaussian.test(x_test,y_test)
-    acc += float(r)/len(x_test)
-    t+=(t2-t1)
-    print("test n {}/{}".format(i,ntest))
-    print("Donnees d'apprentissage : {} ".format(len(x_train)))
-    print("Temps d'apprentissage :{} s".format(t2-t1))
-    print("Donnees de test : {} ".format(len(x_test)))
-    print("Performance :{} % ".format(r*100/len(x_test)))
-    print("gamma :{}  ".format(gaussian.gamma))
-    print("alpha {}  ".format(gaussian.alpha))
-
-print("acc moyenne:",acc/ntest)
-print("temps entrainement moyenn:",t/ntest)
-
-
-x1,y1=[ (0.5*np.random.randn() -1)    for k in range(10)],[ +1 for k in range(10)]
-x2,y2=[ (0.5*np.random.randn()+ 2) for k in range(10)],[ -1 for k in range(10)]
-x3,y3=[ (0.5*np.random.randn()+ 4) for k in range(10) ],[+1 for k in range(10) ]
-
-x=x1+x2+x3
-y=y1+y2+y3
-
-plt.ion()
-plt.figure(1)
-
-gaussian=SDCA(gauss)
-gaussian.gamma = 1.0
-alpha=gaussian.train(x,y,E=10,C=1)
-
-xtest=np.linspace(-3,6,20)
-ytest=[gaussian.predict(xi) for xi in xtest]
-
-
-plt.figure(1)
-
-plt.stem(x1,y1)
-plt.stem(x2,y2)
-plt.stem(x3,y3)
-
-print(len(x),len(y))
-
-plt.stem(x,[y[i]*alpha[i] for i in range(len(alpha))],markerfmt='b+')
-plt.plot(xtest,ytest)
-plt.ioff()
-plt.show()
-"""
--- svm.py
+++ svm.py
@@ -2,23 +2,30 @@
 class svm :
     
     """
+    @brief Fit the SVM model according to the given training data.
+    """
+    def fit(self,X,y):
+        raise NotImplementedError
+    """
+    @brief Make a prediction pour the vector X.
+    """    
+    def predict(self,X):
+        raise NotImplementedError
     
     """
-    def fit(X,y):
-        raise NotImplementedError
-        
-    def predict(X):
-        raise NotImplementedError
-        
-    def score(X,y,metric="accuracy"):
+    @brief Returns the accuracy on the given test data and labels. Other metrics will be implemented in the future. 
+    """
+    def score(self,X,y,metric="accuracy"):
         if(metric=="accuracy"):
             c=0.0
             for i,xi in enumerate(X):
-                if( int(np.sign(self.predict(xi))) == int(np.sign(y[i]))) :
+                if  int(np.sign( self.predict(xi) ) ) == int(np.sign(y[i]))  :
                     c+=1.0
             return c/len(X)
         else :
             raise NotImplementedError
-            
-    def get_params():
+    """
+    @brief Get parameters for this estimator.
+    """
+    def get_params(self):
         raise NotImplementedError
